f5f93d29b04d78830e2a10d80cb74436f272f814
udodinho <udodinho200@gmail.com>
2022-09-22 11:31:26 +0100

    Connect to mongo

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
-const morgan = require("morgan")
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blogs")
 
 const app = express();
 
-app.set("view engine", "ejs")
-
-app.listen(4000, () => {
+// Connect to mongodb
+const dbURI = "mongodb+srv://node-ninja:node1234@nodecourse.poszrnv.mongodb.net/node-tutor?retryWrites=true&w=majority"
+mongoose.connect(dbURI)
+.then((result) => app.listen(4000, 
     console.log("Server started on port 4000")
-});
+))
+.catch((err) => console.log(err))
+app.set("view engine", "ejs")
 
 // Middleware and static files
 app.use(express.static("public"))
