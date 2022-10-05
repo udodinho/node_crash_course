deeeff697f5d1192c8688affb7214f970570a29e
udodinho <udodinho200@gmail.com>
2022-10-05 18:57:47 +0100

    Move blog route to a new file and import it

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blogs");
 
 const app = express();
+const blogRoutes = require("./routes/blogRoutes")
 
 // Connect to mongodb
 const dbURI = "mongodb+srv://node-ninja:node1234@nodecourse.poszrnv.mongodb.net/node-tutor?retryWrites=true&w=majority"
@@ -24,61 +24,12 @@ app.get("/", (req, resp) => {
     resp.redirect("/blogs");
 });
 
-app.post("/blogs", (req, resp) => {
-    const blog = new Blog(req.body);
-
-    blog.save()
-    .then((result) => {
-        resp.redirect("/blogs");
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-});
-
-app.get("/blogs/:id", (req, resp) => {
-    const id = req.params.id;
-
-    Blog.findById(id)
-    .then(result => {
-        resp.render("details", {title: "Blog details", blog: result})
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})
-
-app.delete("/blogs/:id", (req, resp) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-    .then(result => {
-        resp.json({ redirect: "/blogs" });
-        console.log(result)
-    })
-    .catch(err => {
-        console.log(err);
-    })
-})
-
 app.get("/about", (req, resp) => {
     resp.render("about", {title: "About"});
 });
 
 // Blog routes
-app.get("/blogs", (req, resp) => {
-    Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-        resp.render("index", {title: "All blogs", blogs: result });
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-})
-
-app.get("/blogs/create", (req, resp) => {
-    resp.render("create", {title: "Create a new blog"});
-});
+app.use("/blogs", blogRoutes);
 
 app.use( (req, resp) => {
     resp.render("404", {title: "404"});
